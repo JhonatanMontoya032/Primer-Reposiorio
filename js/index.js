c0e0e2fbc55289c9ecc12bebb52639c0bab86410
JhonatanMontoya032 <jhonatanestudios238@gmail.com>
2024-06-07 15:04:56 -0500

    Version 1.0.1

---
+++ js/index.js
@@ -0,0 +1,3 @@
+let nombre = ["Jhonatan","Alejandro","Montoya","Martínez"]
+let datos = ["1011393426", "47"]
+console.log("datos: \n" + "Nombre: " + nombre[0] + "\n" + "Segundo Nombre: " + nombre[1] + "\n" + "Primer Apellido: " + nombre[2] + "\n" + "Segundo Apellido " + nombre[3] + "\n" + "*************************" + "\n" + "Id: " + datos[0] + "\n" + "Número de Ficha: " + datos[1])
